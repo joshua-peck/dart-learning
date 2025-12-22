10f92f82056fe81422cf9abeb3037746c54ff341
Josh Peck <jmp@joshpeck.org>
2025-12-22 08:22:55 -0700

    adding basic types

---
+++ types_basic.dart
@@ -0,0 +1,24 @@
+void main() {
+	// Data Types
+	// Numbers, Strings, Booleans, Lists, Maps, Dynamic
+
+	// Strings
+	String firstName = "James";
+	print("String: $firstName");
+
+	// Integers
+	int answer = 42;
+	print("answer: $answer");
+
+	// Doubles
+	double pi = 3.14159;
+	print("pi: $pi");
+
+	// Booleans
+	bool isLogical = false;
+	print("isLogical: $isLogical");
+
+	// Dynamic
+	dynamic fullName = "James T. Kirk";
+	print("Dynamic: $fullName");
+}
